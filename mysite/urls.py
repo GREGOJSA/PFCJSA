b43da7751648083567e523b590da0789967c0630
joelespa <joel.sparton@gmail.com>
2017-03-25 21:07:25 +0100

    subida con detalle post

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from blog import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list),
+    url(r'^post/(?P<post_id>[0-9]+)/$', views.post_detail),
 ]
