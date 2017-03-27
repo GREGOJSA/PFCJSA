1f2049bf3c67d48a396a7c91abc4c19f8c4204ee
joelespa <joel.sparton@gmail.com>
2017-03-27 21:01:07 +0200

    subidon 2

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ def post_list(request):
         return render(request, 'blog/postlist.html', {'posts' : post})
 
 def post_detail(request, pk):
-        post = Post.objects.get(pk=pk)
+        post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
